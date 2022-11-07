61803f0d7a132ecd4d3b6cb9dab08aefb9a5cefe
Toon Dehaene <dehaene.toon@gmail.com>
2022-11-07 15:37:39 +0100

    start working on server client code

---
+++ 1-Replication/server_client_code/rlnc.py
@@ -0,0 +1,21 @@
+import kodo
+import math
+import random
+import copy # for deepcopy
+from utils import random_string
+import messages_pb2
+import json
+
+STORAGE_NODES_NUM = 4
+
+# TO BE DONE: placeholder for store_file
+
+# TO BE DONE: placeholder for __decode_file
+
+# TO BE DONE: placeholder for recode
+
+# TO BE DONE: placeholder for get_file
+
+# TO BE DONE: placeholder for __store_repair_fragments
+
+# TO BE DONE: placeholder for start_repair_process
